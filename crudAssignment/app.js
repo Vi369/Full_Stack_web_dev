d938bd326572bbe1381dbbf6fb351cc810bbbf7e
Vikas singh varma <Vikashverma3001@gmail.com>
2024-01-01 09:57:26 +0530

    resolve

--- crudAssignment/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-require('dotenv').config();
-const connectToDb = require('./dbConfig/db.js')
-
-const app = express();
-// conneted to database
-connectToDb();
-module.exports = app;
--- crudAssignment/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app.js');
-
-const PORT = process.env.PORT || 6000;
-
-app.listen(PORT, ()=>{
-    console.log(`server is running at http://localhost:${PORT}`);
-})
